bcb0172e799cb2e1be1da61eafd2a74bcbf3ef29
unknown <dips.deeptisharma@gmail.com>
2020-07-05 12:42:22 +0530

    create question 1.0

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -1,29 +1,35 @@
 package com.upgrad.quora.api.controller;
-
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.api.model.QuestionRequest;
+import com.upgrad.quora.api.model.QuestionResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import com.upgrad.quora.service.Business;
 import org.springframework.http.MediaType;
+import com.upgrad.quora.service.business.QuestionService;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class QuestionController {
   
   @Autowired
-  private QuestionBusinessService questionBusinessService;
- /* @RequestMapping(method = RequestMethod.POST, path = "/question/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<QuestionResponse> createQuestion(final QuestionRequest questionRequest) {
+  private QuestionService questionService;
+  @RequestMapping(method = RequestMethod.POST, path = "/question/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> createQuestion(@RequestParam String content) {
 
-        final UserEntity userEntity = new UserEntity();
-
-        final UserEntity createdUserEntity = signupBusinessService.signup(userEntity);
-        QuestionResponse questionResponse = new QuestionResponse().id(createdUserEntity.getUuid()).status("Question created successfully");
-        return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
-    }*/
+        final QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.setContent(content);
+        questionEntity.setDate(new Date("2020-05-07"));
+        questionEntity.setUuid("ui");
+        questionService.createQuestion(questionEntity);
+        return new ResponseEntity<>("Question created successfully", HttpStatus.CREATED);
+    }
   
   /* @RequestMapping(method = RequestMethod.GET, path = "/question/all", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionDetailsResponse> getAllQuestions() {
