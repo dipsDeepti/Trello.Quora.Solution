aea71817570a0e5a1b6f559233a622cf8b79d07c
Sourabh Mehta <sam06mehta@gmail.com>
2020-07-09 02:09:05 +0530

    Data Access Object for User profile Api

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -1,4 +1,36 @@
-package dao;
+package com.upgrad.quora.service.dao;
 
+
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
 public class UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    // getUser method fetches userEntity from database
+    public UserEntity getUser(final String userUuid) {
+        try {
+            return entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", userUuid)
+                    .getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    //getUserAuthToken fetch authentication information form database
+    public UserAuthEntity getUserAuthToken(final String accessToken) {
+        try {
+            return entityManager.createNamedQuery("userAuthTokenByAccessToken", UserAuthEntity.class).setParameter("accessToken", accessToken).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+
+    }
 }
